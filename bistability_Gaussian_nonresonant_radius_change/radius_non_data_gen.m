fcb6fc9bb36342024fb793951163dcfa953f1fef
KODO <kdg930202@naver.com>
2024-07-24 22:16:19 +0900

    Bistability loop by chaning nonresonant Gaussian pump radius

---
+++ bistability_Gaussian_nonresonant_radius_change/radius_non_data_gen.m
@@ -0,0 +1,6 @@
+%How bistability different with nonresonant gaussian 
+
+[l6,r6,Pr_l6,Pr_r6]=radius_non(6);
+[l9,r9,Pr_l9,Pr_r9]=radius_non(9);
+[l12,r12,Pr_l12,Pr_r12]=radius_non(12);
+[l15,r15,Pr_l15,Pr_r15]=radius_non(15);
