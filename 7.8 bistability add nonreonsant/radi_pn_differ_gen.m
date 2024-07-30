a5877038c565cd92573dc56ba9ec0ccba88e6a37
KODO <kdg930202@naver.com>
2024-07-30 21:02:12 +0900

    add

---
+++ 7.8 bistability add nonreonsant/radi_pn_differ_gen.m
@@ -0,0 +1,13 @@
+close all
+% plot(Pr_l6,l6,'b',LineWidth=2)
+% hold on
+% plot(Pr_l15,l15,'r',LineWidth=2)
+% plot(Pr_l24,l24,'g',LineWidth=2)
+% plot(Pr_l_homo,l_homo,'m',LineWidth=2)
+% plot(Pr_r6,r6,'b',LineWidth=2)
+% plot(Pr_r15,r15,'r',LineWidth=2)
+% plot(Pr_r24,r24,'g',LineWidth=2)
+plot(Pr_l_homo,l_homo,'m',LineWidth=2)
+hold on
+plot(Pr_r_homo,r_homo,'m',LineWidth=2)
+% legend(["radi\_{Pn} : 3um","radi\_{Pn} : 7.5um","radi\_{Pn} : 12um","Homogenous"],FontSize=20,Location="southeast")
---
+++ 7.8 bistability add nonreonsant/radius_non_data_gen.m
@@ -0,0 +1,10 @@
+%How bistability different with nonresonant gaussian 
+
+% [l6,r6,Pr_l6,Pr_r6]=radius_non(6);
+% [l9,r9,Pr_l9,Pr_r9]=radius_non(9);
+% [l12,r12,Pr_l12,Pr_r12]=radius_non(12);
+% [l15,r15,Pr_l15,Pr_r15]=radius_non(15);
+
+
+[l24,r24,Pr_24,Pr_r24]=radius_non(24);
+% [l33,r33,Pr_l33,Pr_r33]=radius_non(33);
